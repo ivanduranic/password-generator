d5f65de413cc84e5d10085789646cca62866937d
Ivan Duranic <ivan.duranic@gmail.com>
2020-10-18 19:12:39 -0400

    html, css and js have been made and the page is functional

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,13 @@
-// VARIABLES
-      // ==========================================================================
-      var letter;
+    var letter;
 
       var password = {
-        length: 8,
+        length: [8 - 128],
         uppercase: true,
         lowercase: true,
         number: true,
-        specialCharacter: true,
 
         types: function() {
-          alert("MUST contain at least one uppercase letter, MUST contain at least one lowercase letter, MUST contain at least one number, MUST contain at least one special character (!”#$%&'()*+,-./:;<=>?@[\]^_`{|}~ )!");
+          alert("MUST contain at least one uppercase letter, MUST contain at least one lowercase letter, MUST contain at least one number!");
         },
 
         characters: function() {
@@ -27,7 +24,6 @@
         console.log("Uppercase: " + password.uppercase);
         console.log("Lowercase:" + password.uppercase);
         console.log("Number: " + password.number);
-        console.log("specialCharacter: " + password.specialCharacter);
         console.log("------------------------------");
       }
 
@@ -46,12 +42,67 @@
           reWriteStats();
         }
         // If the letter is t, run the following functions/methods.
-        else if (letter === "t" && password.specialCharacter) {
+        else if (letter === "t") {
           password.types();
           reWriteStats();
         }
 
       }
 
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-      
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
